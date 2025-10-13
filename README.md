9380680e5612efdc3fe2d18c644ea0ff9865de2f
Jon Warrington <jonw@warringtonz.ca>
2025-10-13 10:17:42 -0600

    Update to latest Angular with a new project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CerberusWebsite
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.5.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.5.
 
 ## Development server
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import Material from '@primeng/themes/material';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-      },
-    }),
-  ],
+    provideRouter(routes)
+  ]
 };
