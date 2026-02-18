04735612448a7a7befc7ecd5009e189a3a90bd43
Jon Warrington <jonw@warringtonz.ca>
2026-02-18 14:51:06 -0700

    Adding the Not for Profit name to the copyright in the footer

--- src/app/layout/components/app.footer.ts
+++ src/app/layout/components/app.footer.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-footer',
-  template: `<div class="layout-footer">&copy;{{ currentYear }} USS Cerberus</div>`,
+  template: `<div class="layout-footer">
+    &copy;{{ currentYear }} 2724594 ALBERTA SOCIETY / USS Cerberus
+  </div>`,
 })
 export class AppFooter {
   currentYear: number = new Date().getFullYear();
