58cf1501c0995a288b1e46e59c9387a0ab03b205
Jon Warrington <jonw@ggbetawerks.com>
2026-07-25 12:39:52 -0600

    Added a free primeng license

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,6 +25,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
-    providePrimeNG({ theme: { preset: AuraPurple, options: { darkModeSelector: '.app-dark' } } }),
+    providePrimeNG({
+      theme: { preset: AuraPurple, options: { darkModeSelector: '.app-dark' } },
+      license:
+        'eyJpZCI6IjEyNGU2MzllLTAyZWMtNDE2Ny1iZTVmLWZlODhkOGFiNmM3ZCIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODQ4MTg5ODgsImV4cCI6MTgxNjM1NDk4OH0.j9G0Ghp_8TV33zYOFzKfoGac7c8PN9qqHUX5jggRyoMHfRuBAIWTsWskfD1FZ7tjeSfjSOypCyihg1jCbVzKBw',
+    }),
   ],
 };
