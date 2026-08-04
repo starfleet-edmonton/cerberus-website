43f2313a3ab0d6160aa6551c6fabcd763e5db904
Jon Warrington <jonw@ggbetawerks.com>
2026-08-04 13:59:20 -0600

    Trying an Add to Calendar button

--- src/app/pages/members/events/events.ts
+++ src/app/pages/members/events/events.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { PrivateEventService } from '../../../services/private-events.service';
 import { PrivateEvent } from '../../../models/firestore.model';
+import 'add-to-calendar-button';
 
 @Component({
   selector: 'app-events',
   imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './events.html',
   styleUrl: './events.scss',
   changeDetection: ChangeDetectionStrategy.Eager,
